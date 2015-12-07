5d2b2415800526305dd441377a4844f73c865e9c
Tobias Preuss <tobias.preuss@googlemail.com>
2015-12-07 13:15:41 +0100

    Describe how use dynamic URLs.

--- src/main/java/info/metadude/java/library/brockman/StreamsService.java
+++ src/main/java/info/metadude/java/library/brockman/StreamsService.java
@@ -9,11 +9,6 @@ import java.util.List;
 
 public interface StreamsService {
 
-    // String BASE_URL = "https://streaming.media.ccc.de";
-    // String BASE_URL = "https://gist.githubusercontent.com";
-
-    // @GET("/streams/v1.json")
-    // @GET("/MaZderMind/d5737ab867ade7888cb4/raw/5c0b0f4edfc8e939029b1e539a2ef5757601b205/streams-v1.json")
     @GET
     Call<List<Offer>> getOffers(@Url String url);
 
