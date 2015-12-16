7f636633d0a5fc4e2fdd91a029c67472d033c3e9
Tobias Preuss <tobias.preuss@googlemail.com>
2015-12-16 19:32:58 +0100

    Allow to pass in OkHttpClient from application.
    + Interceptors must now be defined within the application!

--- src/main/java/info/metadude/java/library/brockman/ApiModule.java
+++ src/main/java/info/metadude/java/library/brockman/ApiModule.java
@@ -18,15 +18,14 @@ public final class ApiModule {
     }
 
     public static StreamsService provideStreamsService(
-            final String baseUrl, final List<Interceptor> httpClientInterceptors) {
-        return createRetrofit(baseUrl, httpClientInterceptors)
+            final String baseUrl, final OkHttpClient okHttpClient) {
+        return createRetrofit(baseUrl, okHttpClient)
                 .create(StreamsService.class);
     }
 
-    private static Retrofit createRetrofit(String baseUrl, List<Interceptor> httpClientInterceptors) {
-        OkHttpClient httpClient = new OkHttpClient();
-        if (httpClientInterceptors != null) {
-            httpClient.interceptors().addAll(httpClientInterceptors);
+    private static Retrofit createRetrofit(String baseUrl, OkHttpClient okHttpClient) {
+        if (okHttpClient == null) {
+            okHttpClient = new OkHttpClient();
         }
 
         Moshi moshi = new Moshi.Builder()
@@ -38,7 +37,7 @@ public final class ApiModule {
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .client(httpClient)
+                .client(okHttpClient)
                 .build();
     }
 
